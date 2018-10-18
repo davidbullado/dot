18503454a7edfa9005b8f01e7710b118899dfc2c
David Bullado <david.bullado@gmail.com>
2018-10-18 09:57:09 +0200

    VSCode extension list

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension hollowtree.vue-snippets
+code --install-extension ms-python.python
+code --install-extension ritwickdey.LiveServer
